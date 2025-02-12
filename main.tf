6664b835ba89f2e87f3205563e4c93579a992913
Rajat <rajatsood1804@gmail.com>
2025-02-12 16:33:36 -0500

    Fixed IAM member format in Terraform

--- main.tf
+++ main.tf
@@ -24,5 +24,5 @@ resource "google_storage_bucket" "example" {
 resource "google_project_iam_member" "storage_admin" {
   project = "astute-atlas-448617-n8"
   role    = "roles/storage.admin"
-  member  = "terraform-github@astute-atlas-448617-n8.iam.gserviceaccount.com"
+  member  = "serviceAccount:terraform-github@astute-atlas-448617-n8.iam.gserviceaccount.com"
 }
