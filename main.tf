52e918b950fdd37bd178c1fb8febdab9ba8fa194
Rajat <rajatsood1804@gmail.com>
2025-02-12 15:34:05 -0500

    Fixed Terraform provider issue

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
-  project = "astute-atlas-448617-n8"   
+  project = "your-project-id"   
   region  = "us-central1"
 }
 
 resource "google_storage_bucket" "example" {
-  name     = "my-secure-bucket-1234556"    location = "US"
+  name     = "my-secure-bucket-1234556"
+  location = "US"
 
   versioning {
     enabled = true
